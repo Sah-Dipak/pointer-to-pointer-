d154f1f293b652d722dfdae0f7738bd8f6b9fe4b
Dipak Kumar <dipaksah62008490@gmail.com>
2025-07-21 23:17:42 +0530

    Add files via upload

---
+++ ptr_to_ptr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int a=76;
+    int *ptr1=&a;
+    int **ptr2=&ptr1;
+
+    printf("a=%d\n",a);
+    printf("*ptr1=%d\n",*ptr1);
+    printf("**ptr2=%d\n",**ptr2);
+    
+    return 0;
+
+}
